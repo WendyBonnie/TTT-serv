c8f3c01a65096e54ae084baf37c80864399814e2
wendy <wendymontagnon@gmail.com>
2021-02-24 14:00:43 +0100

    cx

--- src/Components/monProfil/monProfil.js
+++ src/Components/monProfil/monProfil.js
@@ -308,12 +308,12 @@ class Profil extends Component {
           </Col>
         </Row>
         <Row >
-        <Col xs={12} s={12} md={6} lg={6}>
+       
             
             <Col xs={12} s={12} md={6} lg={6}>
             {this.renderCompteReferent()}
           </Col>
-          </Col>
+         
           </Row>
       </Container>
     );
