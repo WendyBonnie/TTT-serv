35bd4374ae2a1a9aea2273d4e484097b72b2ba1a
Lambert-BC <aug.lamb@gmail.com>
2021-07-19 16:47:24 +0200

    build

---
+++ build/precache-manifest.5f4e1f5bc2c4bef3bd546962db39ed22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa905834ddf368c3cea29c61668e0364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27170e4e15e9dd6ac019",
+    "url": "/static/css/2.88c345fd.chunk.css"
+  },
+  {
+    "revision": "cdf2059faa0b2cec86c3",
+    "url": "/static/css/main.d8c045f6.chunk.css"
+  },
+  {
+    "revision": "27170e4e15e9dd6ac019",
+    "url": "/static/js/2.e79cbfe7.chunk.js"
+  },
+  {
+    "revision": "861270dc56fc11dfc4b9511ee8be866b",
+    "url": "/static/js/2.e79cbfe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf2059faa0b2cec86c3",
+    "url": "/static/js/main.903677c1.chunk.js"
+  },
+  {
+    "revision": "694065b0f5781de90917",
+    "url": "/static/js/runtime-main.dbf2c205.js"
+  }
+]);
