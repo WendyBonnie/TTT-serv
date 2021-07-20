802fb4cb42ff5c82921b842b3a880c14f93e6d71
Wendy-M <wendymontagnon@gmail.com>
2021-07-20 11:10:19 +0200

    build

---
+++ build/precache-manifest.d3a95bc22f07498e8ac52a50388af288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889823b7fa500af9bb6cea2498cdc5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27170e4e15e9dd6ac019",
+    "url": "/static/css/2.88c345fd.chunk.css"
+  },
+  {
+    "revision": "4dd2e2918dd4ee0b0817",
+    "url": "/static/css/main.61edc7ef.chunk.css"
+  },
+  {
+    "revision": "27170e4e15e9dd6ac019",
+    "url": "/static/js/2.e79cbfe7.chunk.js"
+  },
+  {
+    "revision": "861270dc56fc11dfc4b9511ee8be866b",
+    "url": "/static/js/2.e79cbfe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd2e2918dd4ee0b0817",
+    "url": "/static/js/main.b0db714c.chunk.js"
+  },
+  {
+    "revision": "694065b0f5781de90917",
+    "url": "/static/js/runtime-main.dbf2c205.js"
+  }
+]);
